7e7f92163c3149d77b86827939c262d0f33b5efd
Paolo Valente <paolo.valente@linaro.org>
2017-04-26 19:14:04 +0200

    block, bfq: use pointer entity->sched_data only if set
    In the function __bfq_deactivate_entity, the pointer
    entity->sched_data could happen to be used before being properly
    initialized. This led to a NULL pointer dereference. This commit fixes
    this bug by just using this pointer only where it is safe to do so.
    
    Reported-by: Tom Harrison <l12436.tw@gmail.com>
    Tested-by: Tom Harrison <l12436.tw@gmail.com>
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- block/bfq-sched.c
+++ block/bfq-sched.c
@@ -1280,13 +1280,22 @@ static bool __bfq_deactivate_entity(struct bfq_entity *entity,
 				    bool ins_into_idle_tree)
 {
 	struct bfq_sched_data *sd = entity->sched_data;
-	struct bfq_service_tree *st = bfq_entity_service_tree(entity);
-	bool is_in_service = entity == sd->in_service_entity;
+	struct bfq_service_tree *st;
+	bool is_in_service;
 
 	if (!entity->on_st) { /* entity never activated, or already inactive */
-		BUG_ON(entity == entity->sched_data->in_service_entity);
+		BUG_ON(sd && entity == sd->in_service_entity);
 		return false;
 	}
+
+	/*
+	 * If we get here, then entity is active, which implies that
+	 * bfq_group_set_parent has already been invoked for the group
+	 * represented by entity. Therefore, the field
+	 * entity->sched_data has been set, and we can safely use it.
+	 */
+	st = bfq_entity_service_tree(entity);
+	is_in_service = entity == sd->in_service_entity;
 
 	BUG_ON(is_in_service && entity->tree && entity->tree != &st->active);
 
