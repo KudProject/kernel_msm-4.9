113606337b59201936e6ef2ad6658487c3ad08e1
Paolo Valente <paolo.valente@linaro.org>
2017-07-03 22:00:29 +0200

    BFQ-v8r12
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5311,7 +5311,7 @@ static struct blkcg_policy blkcg_policy_bfq = {
 static int __init bfq_init(void)
 {
 	int ret;
-	char msg[60] = "BFQ I/O-scheduler: v8r11";
+	char msg[60] = "BFQ I/O-scheduler: v8r12";
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
 	ret = blkcg_policy_register(&blkcg_policy_bfq);
