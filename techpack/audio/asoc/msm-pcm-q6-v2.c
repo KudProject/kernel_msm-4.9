a057679da2525866999995d6ded991463bc1f595
Artem Borisov <dedsa2002@gmail.com>
2018-11-06 15:09:21 +0400

    techpack: asoc: Use v3 format for PCM
    V4 is not supported by old firmware.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- techpack/audio/asoc/msm-pcm-q6-v2.c
+++ techpack/audio/asoc/msm-pcm-q6-v2.c
@@ -387,6 +387,10 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			return -ENOMEM;
 		}
 	} else {
+#if (defined CONFIG_MACH_XIAOMI_SANTONI)
+		ret = q6asm_open_write_v3(prtd->audio_client,
+			fmt_type, bits_per_sample);
+#else
 		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
@@ -395,6 +399,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 		else
 			ret = q6asm_open_write_v4(prtd->audio_client,
 				fmt_type, bits_per_sample);
+#endif
 
 		if (ret < 0) {
 			pr_err("%s: q6asm_open_write failed (%d)\n",
@@ -435,6 +440,13 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			prtd->channel_map, bits_per_sample);
 	} else {
 
+#if (defined CONFIG_MACH_XIAOMI_SANTONI)
+		ret = q6asm_media_format_block_multi_ch_pcm_v3(
+				prtd->audio_client, runtime->rate,
+				runtime->channels, !prtd->set_channel_map,
+				prtd->channel_map, bits_per_sample,
+				sample_word_size);
+#else
 		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2) {
@@ -453,6 +465,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 				sample_word_size, ASM_LITTLE_ENDIAN,
 				DEFAULT_QF);
 		}
+#endif
 	}
 	if (ret < 0)
 		pr_info("%s: CMD Format block failed\n", __func__);
@@ -511,6 +524,10 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 				__func__, params_channels(params),
 				prtd->audio_client->perf_mode);
 
+#if (defined CONFIG_MACH_XIAOMI_SANTONI)
+		ret = q6asm_open_read_v3(prtd->audio_client, FORMAT_LINEAR_PCM,
+				bits_per_sample);
+#else
 		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
@@ -521,6 +538,7 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 			ret = q6asm_open_read_v4(prtd->audio_client,
 				FORMAT_LINEAR_PCM,
 				bits_per_sample, false);
+#endif
 		if (ret < 0) {
 			pr_err("%s: q6asm_open_read failed\n", __func__);
 			q6asm_audio_client_free(prtd->audio_client);
@@ -588,6 +606,13 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 			__func__, prtd->samp_rate, prtd->channel_mode,
 			bits_per_sample, sample_word_size);
 
+#if (defined CONFIG_MACH_XIAOMI_SANTONI)
+	ret = q6asm_enc_cfg_blk_pcm_format_support_v3(prtd->audio_client,
+						      prtd->samp_rate,
+						      prtd->channel_mode,
+						      bits_per_sample,
+						      sample_word_size);
+#else
 	if (q6core_get_avcs_api_version_per_service(
 			APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 			ADSP_ASM_API_VERSION_V2)
@@ -609,6 +634,7 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 						ASM_LITTLE_ENDIAN,
 						DEFAULT_QF);
 
+#endif
 	if (ret < 0)
 		pr_debug("%s: cmd cfg pcm was block failed", __func__);
 
