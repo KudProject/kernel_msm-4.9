4a60e0c851d124085e4189fa4dfaf69f20f0c574
Jack Wu <wjack@google.com>
2019-01-11 15:28:30 +0800

    BACKPORT: power: qpnp-fg / qpnp-fg-gen3: fix soc not keeping full after charging overnight
    The issue happened when the temperature was cool after charge_done.
    (chip->health was not POWER_SUPPLY_HEALTH_GOOD). In this condition,
    chip->charge_full was not set, and then battery level dropped.
    
    Set chip->charge_full to true if chip->health is
    POWER_SUPPLY_HEALTH_GOOD or POWER_SUPPLY_HEALTH_COOL
    or POWER_SUPPLY_HEALTH_WARM to fix it.
    
    Notes:
    	* Backport to qpnp-fg, since mido aren't use qpnp-fg-gen3 and
    	currently we have similiar issue.
    
    Bug: 122704179
    Signed-off-by: Jack Wu <wjack@google.com>
    Change-Id: I4fe2c76b62a49569f099d898443033c865333aaf
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -4028,7 +4028,9 @@ static void status_change_work(struct work_struct *work)
 
 	if (chip->status == POWER_SUPPLY_STATUS_FULL) {
 		if (capacity >= 99 && chip->hold_soc_while_full
-				&& chip->health == POWER_SUPPLY_HEALTH_GOOD) {
+				&& (chip->health == POWER_SUPPLY_HEALTH_GOOD
+				|| chip->health == POWER_SUPPLY_HEALTH_COOL
+				|| chip->health == POWER_SUPPLY_HEALTH_WARM)) {
 			if (fg_debug_mask & FG_STATUS)
 				pr_info("holding soc at 100\n");
 			chip->charge_full = true;
