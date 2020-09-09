8eb23e26a9596949c1b3e5b11db0fc64c97c5719
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-09-09 22:40:58 +0530

    SCRIPTS: Update zipping script to build inline prima non-treble zip from treble build

--- anykernel/build_treble_zip.sh
+++ anykernel/build_treble_zip.sh
@@ -19,4 +19,20 @@ mv Image.gz_treble ./ak_dir_working/Image.gz-dtb && cd ak_dir_working
 zip -r ${pre}_BOOT_MOCHI_TREBLE_`date +%d\.%m\.%Y_%H\:%M\:%S`.zip . -x '*.git*'
 mv *.zip ../out
 cd ..
+
+dtbdir=$kernel_dir/out/arch/arm64/boot/dts/qcom
+
+dtc -I dtb -O dts -o $dtbdir/nontreble-inline-prima.dts $dtbdir/msm8937-pmi8937-qrd-sku2-holland1.dtb
+echo "/ {firmware{android{fstab{vendor{status = \"disabled\";};};};};};" >> $dtbdir/nontreble-inline-prima.dts
+dtc -I dts -O dtb -o $dtbdir/nontreble-inline-prima.dtb $dtbdir/nontreble-inline-prima.dts
+
+rm $dtbdir/../../Image.gz-dtb
+cat $dtbdir/nontreble-inline-prima.dtb >> $dtbdir/../../Image.gz
+mv $dtbdir/../../Image.gz $dtbdir/../../Image.gz-dtb
+cp $image ./Image.gz_nontreble-inline-prima
+mv Image.gz_nontreble-inline-prima ./ak_dir_working/Image.gz-dtb && cd ak_dir_working
+zip -r ${pre}_BOOT_MOCHI_NON_TREBLE_PRIMA_INLINE_`date +%d\.%m\.%Y_%H\:%M\:%S`.zip . -x '*.git*'
+mv *.zip ../out
+cd ..
+
 rm -rf ak_dir_working
