8d990db99d117f8f7cdb539a788a50626b92fb8b
Albert I <kras@raphielgang.org>
2021-03-26 02:46:08 +0800

    usb/gadget: configfs: Use UAPI-defined MAX_USB_STRING_LEN
    This is what upstream expects the code to be.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -146,8 +146,7 @@ struct gadget_config_name {
 	struct list_head list;
 };
 
-#define MAX_USB_STRING_LEN	126
-#define MAX_USB_STRING_WITH_NULL_LEN	(MAX_USB_STRING_LEN+1)
+#define MAX_USB_STRING_WITH_NULL_LEN	(USB_MAX_STRING_LEN+1)
 
 static int usb_string_copy(const char *s, char **s_copy)
 {
@@ -155,7 +154,7 @@ static int usb_string_copy(const char *s, char **s_copy)
 	char *str;
 	char *copy = *s_copy;
 	ret = strlen(s);
-	if (ret > MAX_USB_STRING_LEN)
+	if (ret > USB_MAX_STRING_LEN)
 		return -EOVERFLOW;
 
 	if (copy) {
