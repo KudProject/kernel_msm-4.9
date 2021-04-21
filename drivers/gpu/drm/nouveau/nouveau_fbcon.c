34eccb7b95d817fc00a220ebf6cc8637f58bdebc
Albert I <kras@raphielgang.org>
2021-04-22 01:41:12 +0800

    Revert "drm/nouveau/drm/noveau: fix reference count leak in nouveau_fbcon_open"
    This reverts commit f4605dc3581fc71a5f065e1c0bb6e007fb439ac7.
    
    Link: https://lore.kernel.org/lkml/20210421130105.1226686-1-gregkh@linuxfoundation.org/
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/gpu/drm/nouveau/nouveau_fbcon.c
+++ drivers/gpu/drm/nouveau/nouveau_fbcon.c
@@ -183,10 +183,8 @@ nouveau_fbcon_open(struct fb_info *info, int user)
 	struct nouveau_fbdev *fbcon = info->par;
 	struct nouveau_drm *drm = nouveau_drm(fbcon->dev);
 	int ret = pm_runtime_get_sync(drm->dev->dev);
-	if (ret < 0 && ret != -EACCES) {
-		pm_runtime_put(drm->dev->dev);
+	if (ret < 0 && ret != -EACCES)
 		return ret;
-	}
 	return 0;
 }
 
