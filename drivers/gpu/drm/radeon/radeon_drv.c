0efb0617a9c72b65cd270d3f97558bb39cfa3077
Albert I <kras@raphielgang.org>
2021-04-22 01:43:13 +0800

    Revert "drm/radeon: Fix reference count leaks caused by pm_runtime_get_sync"
    This reverts commit 0ba1cb622ad55cb2f567a92d59a24f30d2c3ae74.
    
    Link: https://lore.kernel.org/lkml/20210421130105.1226686-1-gregkh@linuxfoundation.org/
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -496,10 +496,8 @@ long radeon_drm_ioctl(struct file *filp,
 	long ret;
 	dev = file_priv->minor->dev;
 	ret = pm_runtime_get_sync(dev->dev);
-	if (ret < 0) {
-		pm_runtime_put_autosuspend(dev->dev);
+	if (ret < 0)
 		return ret;
-	}
 
 	ret = drm_ioctl(filp, cmd, arg);
 	
