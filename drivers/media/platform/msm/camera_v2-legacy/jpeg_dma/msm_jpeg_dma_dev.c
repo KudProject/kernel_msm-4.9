9ea2284f39e9921a8bc4f427c98ffe6a98520765
Albert I <kras@raphielgang.org>
2022-01-03 21:45:38 +0800

    camera_v2-legacy: Correct an include
    I missed this...for twenty months.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/media/platform/msm/camera_v2-legacy/jpeg_dma/msm_jpeg_dma_dev.c
+++ drivers/media/platform/msm/camera_v2-legacy/jpeg_dma/msm_jpeg_dma_dev.c
@@ -21,7 +21,7 @@
 #include <media/v4l2-event.h>
 #include <media/videobuf2-core.h>
 #include <media/v4l2-mem2mem.h>
-#include <media/msm_jpeg_dma.h>
+#include <media/msm_jpeg_dma-legacy.h>
 
 #include "msm_jpeg_dma_dev.h"
 #include "msm_jpeg_dma_hw.h"
