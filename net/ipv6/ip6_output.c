ea06fd9079334750e869fc74cbfa0dbc6384147e
Albert I <kras@raphielgang.org>
2022-03-12 00:40:39 +0800

    Revert "ipv6: remove min MTU check for ipsec tunnels"
    This reverts commit 1d73857127cd7c1546601498b791576db2967b14.
    
    Minimum MTU check would be removed completely with Linux v4.9.305.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1282,7 +1282,7 @@ static int ip6_setup_cork(struct sock *sk, struct inet_cork_full *cork,
 		if (np->frag_size)
 			mtu = np->frag_size;
 	}
-	if (!(rt->dst.flags & DST_XFRM_TUNNEL) && mtu < IPV6_MIN_MTU)
+	if (mtu < IPV6_MIN_MTU)
 		return -EINVAL;
 	cork->base.fragsize = mtu;
 	if (dst_allfrag(rt->dst.path))
